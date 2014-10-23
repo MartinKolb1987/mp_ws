14d189a0c26da1ecd2f6349b7bdb9f40c6daa1b8
Blacklist <blacklist.mod@web.de>
2014-10-23 14:37:19 +0200

    added admin.php

---
+++ server/admin.php
@@ -0,0 +1,55 @@
+<?php
+
+/* MusicHive Client Web Service
+ * @package musichive
+ * @author musichive
+ * @version Alpha 1
+ */
+
+// includes
+require_once('db/db.php');
+
+/* updateDownvoteLevel()
+ * Update the different downvote levels - coming from the frontend
+ */
+function updateDownvoteLevel() {
+
+}
+
+/* setInternetAccess()
+ * Switch on/off the internet access - coming from the frontend
+ */
+function setInternetAccess() {
+
+}
+
+/* getBlacklist()
+ * Get the blacklist from the database - coming from the frontend
+ */
+function getBlacklist() {
+
+}
+
+/* banUser()
+ * Set user whose track is currently playing on the blacklist - coming from the frontend
+ */
+function banUser() {
+
+}
+
+/* deleteBannedUser()
+ * Delete user from the blacklist - coming from the frontend
+ */
+function deleteBannedUser() {
+
+}
+
+/* resetPlaylist()
+ * Reset the bucketcontent, downvotes, blacklist, buckets and tracks
+ */
+function resetPlaylist() {
+
+}
+
+
+?>
