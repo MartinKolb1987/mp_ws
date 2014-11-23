3e11338062e3e3169274918d825aea98fd73863c
acidicX <webmaster@carstenm.de>
2014-11-23 23:04:20 +0100

    set correct audio in statup script

--- installer/startup.sh
+++ installer/startup.sh
@@ -7,5 +7,13 @@
 # This is the MusicMagnet startup script
 # Executed via Systemd (mm-startup.service)
 # It will copy the backup database into the RAMDisk
+# and set the correct sound output
 # =======================================================
+# analog out
+amixer cset numid=3 1 > /dev/null
+# HDMI out
+#amixer cset numid=3 3 > /dev/null
+# full output
+amixer sset PCM 95% > /dev/null
+# copy database
 cp -p /usr/share/nginx/html/server/db/db.sqlite /usr/share/nginx/html/server/tmp/db.sqlite
